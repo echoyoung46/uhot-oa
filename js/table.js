1c87061b8e2f0d0e288a48830ae5507a0ec6099f
yangwt <yangwenting@meizu.com>
2015-12-02 20:22:46 +0800

    improve table component

--- js/table.js
+++ js/table.js
@@ -1,39 +1,46 @@
 function Table(){
     this.tableObj = {
-        boxTitle: "",
-        toolList: "",
-        tableTitle: '',
-        tableBody: '',
-        currStatus: 1,
-        currStatus1: -1,
-        currTimeType: "time1",
-        submitStatus: 2,
-        submitTimeType: "time2",
-        index: 1
-    }
+            boxTitle: "",
+            toolList: "",
+            tableTitle: '',
+            tableBody: '',
+            currStatus: 1,
+            currStatus1: -1,
+            currTimeType: "time1",
+            submitStatus: 2,
+            submitTimeType: "time2",
+            index: 1
+        }
+    
 
-    this.drawTable = function(_list,_tableObj){
-        console.log(_list);
-        if(_list != undefined){
+    this.drawTable = function(_arr){
+        var tableDom = '';
+        $.each(_arr, function(index, val) {
+            //bind event
+            this.bindEvent(val.submitStatus, val.submitTimeType);
+
+            //request data
+            var dataArr = this.getList(val.currStatus, val.currStatus1);
+            
+            //data dom
             var bodyDom = '<table>';
-            var domItemArr = _tableObj.tableBody;
-            console.log(domItemArr);
-            for (var i = 0; i < _list.length; i++) {
-                bodyDom += '<tr>';
+            var domItemArr = val.tableBody;
+            for (var i = 0; i < dataArr.length; i++) {
+                bodyDom += '<tr id="' + dataArr[i].design_no + '">';
                 for(var j = 0; j < domItemArr.length; j++){
                     if(domItemArr[j].type == "data"){
                         if(domItemArr[j].val == "d_gender"){
-                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getGender(_list[i][domItemArr[j].val])+ '</td>';
+                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getGender(dataArr[i][domItemArr[j].val])+ '</td>';
                         }else if(domItemArr[j].val == "series_id"){
-                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getSeries(_list[i][domItemArr[j].val])+ '</td>';
+                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getSeries(dataArr[i][domItemArr[j].val])+ '</td>';
                         }else if(domItemArr[j].val.indexOf('time') > -1){
-                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getFormatTime(_list[i][domItemArr[j].val])+ '</td>';
+                            bodyDom += '<td width="' + domItemArr[j].width + '">' + getFormatTime(dataArr[i][domItemArr[j].val])+ '</td>';
                         }else{
-                            bodyDom += '<td width="' + domItemArr[j].width + '">' + _list[i][domItemArr[j].val]+ '</td>';
+                            bodyDom += '<td width="' + domItemArr[j].width + '">' + dataArr[i][domItemArr[j].val]+ '</td>';
                         }
                         
                     }else if(domItemArr[j].type == "button"){
-                        bodyDom += '<td width="' + domItemArr[j].width + '"><span class="operator" id="operator_' + _list[i].design_no + '">' + domItemArr[j].val + '</span></td>';
+                        bodyDom += '<td width="' + domItemArr[j].width + '"><span class="operator" id="operator_' + dataArr[i].design_no + '">' + domItemArr[j].val + '</span></td>';
                     }else if(domItemArr[j].type == "input"){
                         bodyDom += '<td width="' + domItemArr[j].width + '"><input type="text" class="input-box"></td>';
                     }else if(domItemArr[j].type == "sheet"){
@@ -46,12 +53,27 @@ function Table(){
                 bodyDom += '</tr>';
             };
             bodyDom += '</table>';
-            $('.table-body').html(bodyDom);
-        }
-        
+
+            //build dom
+            tableDom += '<div class="table-box">'
+                     +      '<p class="box-title">' + val.boxTitle + '</p>'
+                     +      '<div class="table-title">'
+                     +          '<table>'
+                     +              '<tr>' + val.tableTitle + '</tr>'
+                     +          '</table>'
+                     +      '</div>'
+                     +      '<div class="table-body">'
+                     +          bodyDom
+                     +      '</div>'
+                     +  '</div>';
+        });
+
+        //draw dom
+        $('.tool-list').html(_arr[0].toolList);
+        $('.close-list').html(tableDom);
     }
 
-    this.bindEvent = function(_obj){
+    this.bindEvent = function(_status, _time){
         $('.table-body').on('click', '.operator', function(event) {
             var closeId = parseInt($(this).attr('id').split('_')[1]);
             var selectNoArr = [];
@@ -59,25 +81,15 @@ function Table(){
             $.confirm({
                 "words": _obj.operatorWords,
                 "yesCallback": function(){
-                    top.updateDressStatus(_obj.submitStatus, selectNoArr, _obj.submitTimeType);
+                    top.updateDressStatus(_status, selectNoArr, _time);
                 }
             }); 
             
         });
     }
 
-    this.getList = function(_func,_obj){
-        /*var data = {
-            "ret": 0,
-            "list": [
-                {"no": 1, "style_type": "女装", "series": 1},
-                {"no": 2, "style_type": 2, "series": 2},
-                {"no": 3, "style_type": 3, "series": 3},
-                {"no": 4, "style_type": 4, "series": 4}
-            ]
-        }
-        this.drawTable(data.list);
-        this.bindEvent();*/
+    this.getList = function(_status, _status1){
+        var res = null;
         if(_obj.currStatus1 < 0){
             var ajaxObj = {status: _obj.currStatus};
         }else{
@@ -87,11 +99,11 @@ function Table(){
             url: '../include/schedule/get_dress_by_status.php',
             type: 'GET',
             dataType: 'json',
+            async: false,
             data: ajaxObj
         })
         .done(function(data) {
-            var list = data.list;
-            _func(list,_obj);
+            res = data.list;
         })
         .fail(function() {
             console.log("error");
@@ -99,16 +111,22 @@ function Table(){
         .always(function() {
             console.log("complete");
         });
-        
+        return res;
     }
 
-    this.init = function(_obj){
-        this.tableObj = $.extend(true, this.tableObj, _obj);
-        $('.tool-list').html(this.tableObj.toolList);
+    this.init = function(_objArr){
+        var tableObjArr = [];
+        $.each(_objArr, function(index, val) {
+            var tempObj = $.extend(true, this.tableObj, val);
+            tableObjArr.push(tempObj);
+        });
+
+        this.drawTable(tableObjArr);
+        /*$('.tool-list').html(this.tableObj.toolList);
         $('.box-title').html(this.tableObj.boxTitle);
         $('.table-title tr').html(this.tableObj.tableTitle);
         this.getList(this.drawTable,this.tableObj);
-        this.bindEvent(this.tableObj);
+        this.bindEvent(this.tableObj);*/
     }
 
 
